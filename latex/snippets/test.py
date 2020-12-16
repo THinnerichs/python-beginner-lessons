53ef14e50e7ba7a17a41302408eec4cde9a7746f
cdboer <cdboer@cdboer.de>
2020-12-17 00:39:37 +0100

    Remove snippet directory.

--- latex/snippets/test.py
+++
@@ -1,4 +0,0 @@
-print ("Hello World")
-
-def hallo():
-    print ("Hallo!")
--- latex/snippets/wuerfel.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-class Wuerfel:
-    def __init__(self, seiten):
-        self.seiten = seiten
-
-    def wuerfeln(self):
-        return randint(1, self.seiten)
